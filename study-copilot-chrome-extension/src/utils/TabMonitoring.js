7c21b8ab30f2d40f117a4cf713c75be3c966a63e
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-30 09:22:44 +0530

    Added: `study-copilot-chrome-extension/src/utils/TabMonitoring.js`

---
+++ study-copilot-chrome-extension/src/utils/TabMonitoring.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TabMonitoring = () => {
+  const handleTabChange = () => {
+    // Logic to monitor tab changes and check relevance to study target
+  };
+
+  const handleIrrelevantWebsite = () => {
+    // Logic to display warning in chat popup for visiting irrelevant websites
+  };
+
+  return (
+    <div>
+      {/* Component logic goes here */}
+    </div>
+  );
+};
+
+export default TabMonitoring;
